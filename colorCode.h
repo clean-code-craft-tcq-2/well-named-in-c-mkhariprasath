d2eb1545825111b5d2b2abd06bd984d47ac47a58
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-01-13 11:02:40 +0530

    Update colorCode.h

--- colorCode.h
+++ colorCode.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <assert.h>
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
@@ -12,8 +14,10 @@ const char* MinorColorNames[] = {
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-    
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
